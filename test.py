9c261f2bdbd487480ad673cb4419f96f0c9588a0
alex <106116143+maslov-cool@users.noreply.github.com>
2025-01-27 21:02:58 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+
+
+def human_read_format(size):
+    sizes = ['Б', 'КБ', 'МБ', 'ГБ']
+    n = size
+    i = 0
+    while n // 1024:
+        i += 1
+        n //= 1024
+    return str(round(size / 1024 ** i)) + sizes[i]
+
+
+def get_files_sizes():
+    return '\n'.join([i + ' ' + human_read_format(os.path.getsize(i)) for i in os.listdir() if os.path.isfile(i)])
+
+
+S
